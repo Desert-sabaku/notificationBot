6a8d1295e114f8c1ae950a3a1e4e107954191a01
SANADA Euki <yuu.mat.930@gmail.com>
2026-05-22 19:24:17 +0900

    feat: Add initial project files including .gitignore, appsscript.json, bun.lock, package.json, and main script

---
+++ code.js
@@ -0,0 +1,80 @@
+function fetchSchedule(calendarId) {
+  const calendar = CalendarApp.getCalendarById(calendarId);
+  const events = calendar.getEvents(...getNextDay());
+
+  if (events.length === 0) return;
+
+  const schedules = events.map(event => {
+    const options = {
+      hour: "numeric",
+      minute: "numeric",
+    };
+ 
+    const startTime = new Intl.DateTimeFormat("ja-JP", options).format(event.getStartTime());
+    const endTime = new Intl.DateTimeFormat("ja-JP", options).format(event.getEndTime());
+    const title = event.getTitle();
+    const desc = event.getDescription() || null;
+    const location = event.getLocation() || null;
+    return `【${startTime}~${endTime}】${title} - ${desc} @${location}`;
+  });
+
+  return schedules;
+}
+
+function getNextDay() {
+  const today = new Date();
+
+  const nextDayStart = new Date(today);
+  nextDayStart.setDate(today.getDate() + 1);
+  nextDayStart.setHours(0, 0, 0, 0);
+
+  const nextDayEnd = new Date(today);
+  nextDayEnd.setDate(today.getDate() + 1);
+  nextDayEnd.setHours(23, 59, 59, 999);
+
+  return [nextDayStart, nextDayEnd];
+}
+
+function buildContent(schedules, quote) {
+  const quote_content = quote.join("―");
+  const head_content = "明日の予定だ";
+  const schedule_content = schedules.join("\n");
+  return [quote_content, head_content, schedule_content].join("\n");
+}
+
+function postToDiscord(webhookUrl, content) {
+  const payload = {
+    content: content,
+  };
+
+  const options = {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify(payload),
+    muteHttpExceptions: true
+  };
+
+  const response = UrlFetchApp.fetch(webhookUrl, options);
+  return response.getContentText();
+}
+
+function fetchQuote() {
+  // このJSONの解析はzenquotesに依存しているため、URLだけ外部から渡しても意味が薄い。
+  // したがってURLは内部で定義する。
+  const url = PropertiesService.getScriptProperties().getProperty("QUOTE_URL");
+
+  const response = UrlFetchApp.fetch(url);
+  const data = JSON.parse(response.getContentText())[0]; // 配列の形式になっているため。
+  const quote = LanguageApp.translate(data.q, "en", "ja");
+  const author = LanguageApp.translate(data.a, "en", "ja");
+  console.log(quote, author);
+  return [quote, author];
+}
+
+function main() {
+  const calendarId = PropertiesService.getScriptProperties().getProperty("CALENDAR_ID");
+  const webhookUrl = PropertiesService.getScriptProperties().getProperty("WEBHOOK_URL");
+  const schedule = fetchSchedule(calendarId);
+  const quote = fetchQuote();
+  postToDiscord(webhookUrl, buildContent(schedule, quote));
+}
