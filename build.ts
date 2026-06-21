386a0dee73e23176c6f706e357d210bb9df25096
SANADA Euki <yuu.mat.930@gmail.com>
2026-06-21 18:08:30 +0900

    chore: Change the transpile library

--- build.ts
+++ build.ts
@@ -1,10 +1,12 @@
 import esbuild from "esbuild";
-import { GasPlugin } from "esbuild-gas-plugin";
+import GasPlugin from "esbuild-plugin-gas-generator";
 
 esbuild.build({
     entryPoints: ["src/main.ts"],
     bundle: true,
+    format: "iife",
+    metafile: true,
     outfile: "dist/main.js",
-    target: "es2021", // GASの処理系の仕様に合わせる
-    plugins: [GasPlugin],
+    target: "es2021",
+    plugins: [GasPlugin({ appsscript: "appsscript.json" })],
 }).catch(() => process.exit(1));
